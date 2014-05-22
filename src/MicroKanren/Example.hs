bc98def104dbf1fa44557df82a8f89a87fc674f8
Oregu <oleg.burykin@gmail.com>
2014-05-23 00:21:16 +0400

    Its polymorphic now, so integers look better

--- src/MicroKanren/Example.hs
+++ src/MicroKanren/Example.hs
@@ -5,20 +5,20 @@ import MicroKanren
 emptyS ∷ ([Subst a], Integer)
 emptyS = ([], 0)
 
-aANDb ∷ ([Subst String], Integer) → [([Subst String], Integer)]
+aANDb ∷ ([Subst Integer], Integer) → [([Subst Integer], Integer)]
 aANDb = conj
-  (callFresh (\a -> a === Val "7"))
-  (callFresh (\b -> disj (b === Val "5") (b === Val "6")))
+  (callFresh (\a -> a === Val 7))
+  (callFresh (\b -> disj (b === Val 5) (b === Val 6)))
 
-ex1, ex2 ∷ [([Subst String], Integer)]
-ex1 = callFresh (\q -> q === Val "5") emptyS
+ex1, ex2 ∷ [([Subst Integer], Integer)]
+ex1 = callFresh (\q -> q === Val 5) emptyS
 ex2 = aANDb emptyS
 
-fives, sixes ∷ Var String -> ([Subst String], Integer) -> [([Subst String], Integer)]
-fives x = disj (x === Val "5") (fives x)
-sixes x = disj (x === Val "6") (sixes x)
+fives, sixes ∷ Var Integer -> ([Subst Integer], Integer) -> [([Subst Integer], Integer)]
+fives x = disj (x === Val 5) (fives x)
+sixes x = disj (x === Val 6) (sixes x)
 
-runFives, fivesAndSixes ∷ [([Subst String], Integer)]
+runFives, fivesAndSixes ∷ [([Subst Integer], Integer)]
 runFives = callFresh fives emptyS
 fivesAndSixes = callFresh (\x -> disj (fives x) (sixes x)) emptyS
 
