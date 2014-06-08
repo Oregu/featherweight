fa44e3b6911e8febe17844082bf2a598060dc003
Oregu <oleg.burykin@gmail.com>
2014-06-08 15:36:49 +0400

    Monadic implementation finished (same as non-monadic)

--- src/MicroKanren/ExampleMonadic.hs
+++ src/MicroKanren/ExampleMonadic.hs
@@ -2,25 +2,25 @@ module MicroKanren.ExampleMonadic where
 
 import Control.Monad
 
-import MicroKanren (LVar(LVar, LVal), Subst)
+import MicroKanren (LVar(LVal))
 import MicroKanren.Monad
 
-emptyS ∷ (Logic a, Integer)
+emptyS ∷ SC α
 emptyS = (mzero, 0)
 
-aANDb ∷ (Logic Integer, Integer) → [(Logic Integer, Integer)]
+aANDb ∷ Goal Integer
 aANDb = conj
-  (callFresh (\a -> a === Val 7))
-  (callFresh (\b -> disj (b === Val 5) (b === Val 6)))
+  (callFresh (\a -> a === LVal 7))
+  (callFresh (\b -> disj (b === LVal 5) (b === LVal 6)))
 
-ex1, ex2 ∷ [(Logic Integer, Integer)]
-ex1 = callFresh (\q -> q === Val 5) emptyS
-ex2 = aANDb emptyS
+runEx1, runEx2 ∷ Logic (SC Integer)
+runEx1 = callFresh (\q -> q === LVal 5) emptyS
+runEx2 = aANDb emptyS
 
-fives, sixes ∷ Var Integer -> (Logic Integer, Integer) -> [(Logic Integer, Integer)]
-fives x = disj (x === Val 5) (fives x)
-sixes x = disj (x === Val 6) (sixes x)
+fives, sixes ∷ LVar Integer -> Goal Integer
+fives x = disj (x === LVal 5) (fives x)
+sixes x = disj (x === LVal 6) (sixes x)
 
-runFives, fivesAndSixes ∷ Logic Integer
+runFives, run5and6 ∷ Logic (SC Integer)
 runFives = callFresh fives emptyS
-fivesAndSixes = callFresh (\x -> disj (fives x) (sixes x)) emptyS
+run5and6 = callFresh (\x -> disj (fives x) (sixes x)) emptyS
--- src/MicroKanren/Monad.hs
+++ src/MicroKanren/Monad.hs
@@ -2,13 +2,12 @@ module MicroKanren.Monad where
 
 import Control.Monad
 
-import MicroKanren (LVar(LVar, LVal), CanUnify, Subst, unifyTerm, walk)
+import MicroKanren (LVar(LVar), CanUnify, Subst, unify)
 
-newtype Logic α = Logic [α]
-unLogic (Logic a) = a
+newtype Logic α = Logic { unLogic ∷ [α]} deriving Show
 
 type SC α    = ([Subst α], Integer)
-type Goal α  = (SC α → Logic α)
+type Goal α  = (SC α → Logic (SC α))
 
 instance Monad Logic where
   Logic s >>= g = Logic (concat $ map (unLogic . g) s)
@@ -21,19 +20,8 @@ instance MonadPlus Logic where
 extS ∷ LVar a → LVar a → [Subst a] → [Subst a]
 extS x v = (:) (x, v)
 
-(===) ∷ (Eq α, CanUnify α, SC β) ⇒ LVar α → LVar α → β → Logic β
+(===) ∷ (Eq α, CanUnify α) ⇒ LVar α → LVar α → SC α → Logic (SC α)
 (===) u v sc = maybe mzero (\s → return (s, snd sc)) $ unify u v (fst sc)
-
-unify ∷ (Eq α, CanUnify α) ⇒ LVar α → LVar α → [Subst α] → Maybe [Subst α]
-unify u v s = unify' u' v' s
-  where
-    u' = walk u s
-    v' = walk v s
-    unify' u2@(LVar _) v2@(LVar _) s2 = Just $ if u2 == v2 then s2
-                                                           else extS u2 v2 s2
-    unify' u2@(LVar _) v2          s2 = Just $ extS u2 v2 s2
-    unify' u2         v2@(LVar _)  s2 = Just $ extS v2 u2 s2
-    unify' (LVal u2)   (LVal v2)   s2 =   unifyTerm u2 v2 s2
 
 disj ∷ Goal a → Goal a → Goal a
 disj g1 g2 sc = g1 sc `mplus` g2 sc
