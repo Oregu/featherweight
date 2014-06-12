4a58f716ac050566bda34433cdb79fea5bb0c986
Oregu <oleg.burykin@gmail.com>
2014-06-12 23:05:37 +0400

    Ok, I got monadic code right. Finally. (But I lose fair disjunction in previous commits)

--- src/MicroKanren/ExampleMonadic.hs
+++ src/MicroKanren/ExampleMonadic.hs
@@ -3,7 +3,7 @@ module MicroKanren.ExampleMonadic where
 import Control.Monad
 import Control.Monad.State.Lazy
 
-import MicroKanren (LVar(LVal))
+import MicroKanren (LVar(LVal), LCons(LCons, LCell, Nil))
 import MicroKanren.Monad
 
 emptyS ∷ SC α
@@ -13,23 +13,39 @@ aANDb ∷ Logic (LVar Integer)
 aANDb = do
     a ← fresh
     b ← fresh
-    (a === LVal 7)
-    (mplus (b === LVal 5)
-           (b === LVal 6))
+    a === LVal 7
+    mplus (b === LVal 5)
+          (b === LVal 6)
 
-run ∷ StateT (SC α) L α -> L (α, SC α)
-run g = runStateT g emptyS
+run ∷ Logic α → [SC α]
+run l = map snd $ runStateT l emptyS
 
-runEx1, runEx2 ∷ L (LVar Integer, SC (LVar Integer))
+runEx1, runEx2 ∷ [SC (LVar Integer)]
 runEx1 = run $ do
     q ← fresh
-    (q === LVal 5) ∷ Logic (LVar Integer)
+    q === (LVal 5 ∷ LVar Integer)
 runEx2 = run aANDb
 
---fives, sixes ∷ LVar Integer -> Goal Integer
---fives x = mplus (x === LVal 5) (fives x)
---sixes x = mplus (x === LVal 6) (sixes x)
+fives, sixes ∷ LVar Integer → Logic (LVar Integer)
+fives x = mplus (x === LVal 5) (fives x)
+sixes x = mplus (x === LVal 6) (sixes x)
 
---runFives, run5and6 ∷ Logic (SC Integer)
---runFives = callFresh fives emptyS
---run5and6 = callFresh (\x -> mplus (fives x) (sixes x)) emptyS
+runFives, run5and6 ∷ [SC (LVar Integer)]
+runFives = run $ fresh >>= fives
+run5and6 = run $ do x ← fresh
+                    fives x `mplus` sixes x
+
+appendo ∷ Eq α ⇒ LVar (LCons α) → LVar (LCons α) → LVar (LCons α) → Logic (LVar (LCons α))
+appendo l s out = mplus
+    (do l === LVal Nil
+        s === out)
+    (do h ← fresh
+        t ← fresh
+        l === LVal (LCons h t)
+        res ← fresh
+        out === LVal (LCons h res)
+        appendo t s res)
+
+runAppendo = run $ do
+                   q ← fresh
+                   appendo q q (LVal (LCons (LVal (LCell 1)) (LVal (LCons (LVal (LCell 1)) (LVal Nil)))))
--- src/MicroKanren/Monad.hs
+++ src/MicroKanren/Monad.hs
@@ -1,40 +1,20 @@
 module MicroKanren.Monad where
 
-import Control.Applicative
 import Control.Monad
-import Control.Monad.State.Lazy
+import Control.Monad.State
 
 import MicroKanren (LVar(LVar), CanUnify, unify)
 
-newtype L α = L { unL ∷ [α] } deriving (Show, Functor, Applicative, Alternative)
-type Logic α = StateT (SC α) L α
-
---data Logic α = Logic { getSC ∷ [SC α], getVal ∷ α } deriving (Eq, Show)
+type Logic α = StateT (SC α) [] α
 
 type Subst α = (α, α)
-type SC α    = ([Subst α], Integer)
-
-instance Monad L where
-  L s >>= g = L $ concat $ map (unL . g) s
-  return sc = L [sc]
-
-instance MonadPlus L where
-  mzero = L []
-  mplus (L s1) (L s2) = L $ if null s1 then s2 else head s1 : mplus s2 (tail s1)
-
---instance Monad Logic where
---  Logic s _ >>= g = Logic $ concat $ map (getSC . g) s
---  return sc = Logic ([sc], fresh)
-
---instance MonadPlus Logic where
---  mzero = Logic []
---  mplus (Logic s1) (Logic s2) = Logic $ if null (getSC s1) then s2 else head s1 : mplus s2 (tail s1)
+type SC    α = ([Subst α], Integer)
 
 (===) ∷ (Eq α, CanUnify α) ⇒ LVar α → LVar α → Logic (LVar α)
 (===) u v = do
-    sc <- get
-    maybe mzero (\s → return (s, snd sc)) $ unify u v (fst sc)
-    return (LVar 1)
+            (s, c) ← get
+            maybe mzero (\s' → put (s', c)) $ unify u v s
+            return u
 
 fresh ∷ Logic (LVar α)
 fresh = do (_, cv) ← get
